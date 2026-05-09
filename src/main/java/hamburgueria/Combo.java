60387f23f9795078ae5fc68cb01f9df7429662eb
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-08 22:47:31 -0300

    Criando classe Combo - Composite

---
+++ src/main/java/hamburgueria/Combo.java
@@ -0,0 +1,41 @@
+package hamburgueria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combo implements Item {
+    private String nomeCombo;
+    private List<Item> itensCombo = new ArrayList<>();
+    private float desconto; // Ex: 0.10 para 10% de desconto
+
+    public Combo(String nomeCombo, float desconto) {
+        this.nomeCombo = nomeCombo;
+        this.desconto = desconto;
+    }
+
+    public void adicionarItemCombo(Item item) {
+        this.itensCombo.add(item);
+    }
+
+    @Override
+    public float getPreco() {
+        float total = 0;
+        for (Item item : itensCombo) {
+            total += item.getPreco(); // Recursividade do Composite!
+        }
+        return total - (total * desconto);
+    }
+
+    @Override
+    public String getDescricao() {
+        StringBuilder descricao = new StringBuilder(nomeCombo + " (");
+        for (int i = 0; i < itensCombo.size(); i++) {
+            descricao.append(itensCombo.get(i).getDescricao());
+            if (i < itensCombo.size() - 1) {
+                descricao.append(" + ");
+            }
+        }
+        descricao.append(")");
+        return descricao.toString();
+    }
+}
