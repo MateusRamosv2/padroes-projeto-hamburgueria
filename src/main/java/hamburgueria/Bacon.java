c32b0f4c80b747dc404094c26291e0cda5616128
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-14 21:31:10 -0300

    Adicionando decorators de adicionais - Classe Bacon

--- src/main/java/hamburgueria/Bacon.java
+++ src/main/java/hamburgueria/Bacon.java
@@ -4,4 +4,10 @@ public class Bacon extends AdicionalDecorator {
     public Bacon(Item itemDecorado) { super(itemDecorado); }
     public String getNomeAdicional() { return "Bacon"; }
     public float getPrecoAdicional() { return 5.0f; }
+
+    @Override
+    public String aceitar(VisitorItem visitor) {
+        return visitor.visitar(this);
+    }
+
 }
