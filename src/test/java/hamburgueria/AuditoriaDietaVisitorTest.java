2477a8ab9119b3111d4da2a60e38e265fe29f87b
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-14 22:09:32 -0300

    consolidando os testes de integração e validação do funcionamento total do AuditoriaDietaVisitor

--- src/test/java/hamburgueria/AuditoriaDietaVisitorTest.java
+++ src/test/java/hamburgueria/AuditoriaDietaVisitorTest.java
@@ -14,4 +14,27 @@ class AuditoriaDietaVisitorTest {
 
         assertEquals("[Carnívoro/Porco] Adicional de Bacon -> [Carnívoro] Hambúrguer Bovino Tradicional", resultado);
     }
+
+    @Test
+    void deveAuditarComboInteiroEVerificarAlergenicos() {
+
+        Combo combo = new Combo("Combo Monstro", 0.10f);
+
+
+        combo.adicionarItemCombo(new Queijo(new HamburguerCarne()));
+
+
+        combo.adicionarItemCombo(new BatataFrita());
+        combo.adicionarItemCombo(new Refrigerante());
+
+        AuditoriaDietaVisitor auditor = new AuditoriaDietaVisitor();
+        String relatorioTotem = combo.aceitar(auditor);
+
+
+        assertTrue(relatorioTotem.contains("--- Auditoria do Combo Monstro"));
+        assertTrue(relatorioTotem.contains("[Vegetariano/Laticínio] Adicional de Queijo"));
+        assertTrue(relatorioTotem.contains("[Carnívoro] Hambúrguer Bovino Tradicional"));
+        assertTrue(relatorioTotem.contains("[Vegano] Batata Frita"));
+    }
+
 }
