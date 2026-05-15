9973690792ebffdd6a8ca8a84e1a0954aa4e3933
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-14 21:28:44 -0300

    Implementando HamburguerCarne

--- src/main/java/hamburgueria/HamburguerCarne.java
+++ src/main/java/hamburgueria/HamburguerCarne.java
@@ -3,4 +3,9 @@ package hamburgueria;
 public class HamburguerCarne implements Item {
     public String getDescricao() { return "Hambúrguer de Carne"; }
     public float getPreco() { return 25.0f; }
+
+    @Override
+    public String aceitar(VisitorItem visitor) {
+        return visitor.visitar(this);
+    }
 }
