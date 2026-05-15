ec72b49d409b6ddb109cb33a1f5dc200a86ab041
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-14 21:33:07 -0300

    adicionando decorators de adicionais - Classe Queijo

--- src/main/java/hamburgueria/Queijo.java
+++ src/main/java/hamburgueria/Queijo.java
@@ -4,4 +4,10 @@ public class Queijo extends AdicionalDecorator {
     public Queijo(Item itemDecorado) { super(itemDecorado); }
     public String getNomeAdicional() { return "Queijo"; }
     public float getPrecoAdicional() { return 3.0f; }
+
+    @Override
+    public String aceitar(VisitorItem visitor) {
+        return visitor.visitar(this);
+    }
+
 }
