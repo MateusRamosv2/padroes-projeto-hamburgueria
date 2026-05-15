e4ef268e2101f1971a07c847d08f479f17453b2e
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-14 22:05:54 -0300

    implementando contrato do Visitor no item HamburguerVegano

--- src/main/java/hamburgueria/HamburguerVegano.java
+++ src/main/java/hamburgueria/HamburguerVegano.java
@@ -3,4 +3,9 @@ package hamburgueria;
 public class HamburguerVegano implements Item {
     public String getDescricao() { return "Hambúrguer de Grão de Bico"; }
     public float getPreco() { return 28.0f; }
+
+    @Override
+    public String aceitar(VisitorItem visitor) {
+        return visitor.visitar(this);
+    }
 }
