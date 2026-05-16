c0a07e73b6f66bba69676a68a9bfc1c58c3a858f
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-16 09:41:52 -0300

    Incluindo informações da classe CarrinhoMemento

--- src/main/java/hamburgueria/CarrinhoMemento.java
+++ src/main/java/hamburgueria/CarrinhoMemento.java
@@ -1,4 +1,17 @@
 package hamburgueria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoMemento {
+
+    private final List<Item> itensCapturados;
+
+    public CarrinhoMemento(List<Item> itens) {
+        this.itensCapturados = new ArrayList<>(itens);
+    }
+
+    public List<Item> getItensCapturados() {
+        return this.itensCapturados;
+    }
 }
