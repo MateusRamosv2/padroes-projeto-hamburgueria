fcfc5b801133921eecbb5b031a11d58c259a6318
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 20:42:39 -0300

    Criando fabrica de cache para os detalhes dos itens - Flyweight

--- src/main/java/hamburgueria/DetalheItemFactory.java
+++ src/main/java/hamburgueria/DetalheItemFactory.java
@@ -1,4 +1,24 @@
 package hamburgueria;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DetalheItemFactory {
+
+    private static Map<String, DetalheItem> cacheDetalhes = new HashMap<>();
+
+    public static DetalheItem getDetalhe(String nome, float precoBase, String urlImagem, String dadosNutricionais) {
+        DetalheItem detalhe = cacheDetalhes.get(nome);
+
+        if (detalhe == null) {
+            detalhe = new DetalheItem(nome, precoBase, urlImagem, dadosNutricionais);
+            cacheDetalhes.put(nome, detalhe);
+        }
+
+        return detalhe;
+    }
+
+    public static int getTotalDetalhesEmMemoria() {
+        return cacheDetalhes.size();
+    }
 }
