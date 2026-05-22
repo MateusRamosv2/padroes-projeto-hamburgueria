7927ca78fe23a13852088c5739f786a633c7dadc
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 19:42:33 -0300

    Criando classe Cardápio para gerenciar e clonar os combos disponiveis - Prototype

--- src/main/java/hamburgueria/Cardapio.java
+++ src/main/java/hamburgueria/Cardapio.java
@@ -1,4 +1,23 @@
 package hamburgueria;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cardapio {
+
+    // O nosso catálogo de Protótipos
+    private Map<String, Combo> combosProntos = new HashMap<>();
+
+    public void cadastrarComboPrototype(String chave, Combo combo) {
+        combosProntos.put(chave, combo);
+    }
+
+    // Quando o cliente pede um combo, ele NUNCA recebe o original, ele recebe um CLONE!
+    public Combo solicitarCombo(String chave) {
+        Combo prototipo = combosProntos.get(chave);
+        if (prototipo == null) {
+            throw new IllegalArgumentException("Combo não encontrado no cardápio!");
+        }
+        return prototipo.clone();
+    }
 }
