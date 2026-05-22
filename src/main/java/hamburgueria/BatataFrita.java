ca42402c001954256f699769d6d62a2c4a7258a7
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 20:44:28 -0300

    Atualizando BatataFrita para utilizar o cache da fabrica - Flyweight

--- src/main/java/hamburgueria/BatataFrita.java
+++ src/main/java/hamburgueria/BatataFrita.java
@@ -1,14 +1,27 @@
 package hamburgueria;
 
 public class BatataFrita implements Item {
+
+    // Referência para o objeto leve compartilhado (Flyweight)
+    private final DetalheItem detalhePesado;
+
+    public BatataFrita() {
+        this.detalhePesado = DetalheItemFactory.getDetalhe(
+                "Batata Frita Média",
+                10.0f,
+                "https://cdn.hamburgueria.com/img/batata_media_4k.png",
+                "Porção de 150g de batata palito frita. 312Kcal."
+        );
+    }
+
     @Override
     public float getPreco() {
-        return 10.0f;
+        return this.detalhePesado.getPrecoBase();
     }
 
     @Override
     public String getDescricao() {
-        return "Batata Frita Média";
+        return this.detalhePesado.getNome();
     }
 
     @Override
