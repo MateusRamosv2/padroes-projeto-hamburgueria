8ff85e96622a7b7e7da8a30e38a022b4517d6db8
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 20:43:47 -0300

    Atualizando HamburguerCarne para utilizar o cache da fabrica - Flyweight

--- src/main/java/hamburgueria/HamburguerCarne.java
+++ src/main/java/hamburgueria/HamburguerCarne.java
@@ -1,8 +1,27 @@
 package hamburgueria;
 
 public class HamburguerCarne implements Item {
-    public String getDescricao() { return "Hambúrguer de Carne"; }
-    public float getPreco() { return 25.0f; }
+
+    // Referência para o objeto leve compartilhado (Flyweight)
+    private final DetalheItem detalhePesado;
+
+    public HamburguerCarne() {
+
+        this.detalhePesado = DetalheItemFactory.getDetalhe(
+                "Hambúrguer de Carne",
+                25.0f,
+                "https://cdn.hamburgueria.com/img/carne_4k.png",
+                "Blend 200g, Pão Brioche. 600Kcal. Contém Glúten e Lactose."
+        );
+    }
+
+    public String getDescricao() {
+        return this.detalhePesado.getNome();
+    }
+
+    public float getPreco() {
+        return this.detalhePesado.getPrecoBase();
+    }
 
     @Override
     public String aceitar(VisitorItem visitor) {
