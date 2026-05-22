01cbc59263f2acba35886fbf7e09cf4ee09d7cc3
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 17:57:11 -0300

    Adicionando teste para iterar ignorando pedidos cancelados - Iterator

--- src/test/java/hamburgueria/MonitorCozinhaTest.java
+++ src/test/java/hamburgueria/MonitorCozinhaTest.java
@@ -1,4 +1,42 @@
 package hamburgueria;
 
-public class MonitorCozinhaTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonitorCozinhaTest {
+
+    @Test
+    void deveIterarIgnorandoPedidosCancelados() {
+        MonitorCozinha monitor = new MonitorCozinha();
+
+        Pedido p1 = new PedidoBuilder()
+                .setTipoPedido("Balcao")
+                .setFormaPagamento(new PagamentoPix())
+                .adicionarItem(new HamburguerCarne())
+                .build();
+        monitor.receberPedido(p1);
+
+        Pedido p2 = new PedidoBuilder()
+                .setTipoPedido("Delivery")
+                .setFormaPagamento(new PagamentoPix())
+                .adicionarItem(new BatataFrita())
+                .build();
+        p2.cancelarPedido();
+        monitor.receberPedido(p2);
+
+        Pedido p3 = new PedidoBuilder()
+                .setTipoPedido("Balcao")
+                .setFormaPagamento(new PagamentoPix())
+                .adicionarItem(new Refrigerante())
+                .build();
+        monitor.receberPedido(p3);
+
+        int contagemCozinha = 0;
+        for (Pedido pedidoVisivel : monitor) {
+            contagemCozinha++;
+            assertFalse(pedidoVisivel.isCancelado());
+        }
+
+        assertEquals(2, contagemCozinha);
+    }
 }
