328fe619db4a555ac5c865e5db38a9c488256889
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 20:45:49 -0300

    Criando teste de validação olhando a  economia de memoria e integridade dos itens - Flyweight

--- src/test/java/hamburgueria/DetalheItemFactoryTest.java
+++ src/test/java/hamburgueria/DetalheItemFactoryTest.java
@@ -1,4 +1,27 @@
 package hamburgueria;
 
-public class DetalheItemFactoryTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetalheItemFactoryTest {
+
+    @Test
+    void deveCompartilharDetalhesPesadosNaMemoriaParaMilharesDePedidos() {
+
+        Item pedidoCliente1 = new HamburguerCarne();
+        Item pedidoCliente2 = new HamburguerCarne();
+        Item pedidoCliente3 = new BatataFrita();
+        Item pedidoCliente4 = new HamburguerCarne();
+        Item pedidoCliente5 = new BatataFrita();
+
+
+        assertEquals(2, DetalheItemFactory.getTotalDetalhesEmMemoria());
+
+
+        assertEquals("Hambúrguer de Carne", pedidoCliente1.getDescricao());
+        assertEquals(25.0f, pedidoCliente1.getPreco());
+
+        assertEquals("Batata Frita Média", pedidoCliente3.getDescricao());
+        assertEquals(10.0f, pedidoCliente3.getPreco());
+    }
 }
