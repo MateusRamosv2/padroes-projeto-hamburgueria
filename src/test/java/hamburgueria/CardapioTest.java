3ef2d8ab3c149ebafc60ed3e09a6adfd0700105e
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 19:43:36 -0300

    adicionando testes de customizacao e copia profunda de combos - Prototype

--- src/test/java/hamburgueria/CardapioTest.java
+++ src/test/java/hamburgueria/CardapioTest.java
@@ -1,4 +1,44 @@
 package hamburgueria;
 
-public class CardapioTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardapioTest {
+
+    @Test
+    void deveClonarComboPermitindoCustomizacaoSemAfetarOOriginal() {
+        Cardapio cardapio = new Cardapio();
+
+        Combo comboMaster = new Combo("Combo Casal", 0.10f);
+        BatataFrita batata = new BatataFrita();
+        Refrigerante refri = new Refrigerante();
+
+        comboMaster.adicionarItemCombo(new HamburguerCarne());
+        comboMaster.adicionarItemCombo(new HamburguerCarne());
+        comboMaster.adicionarItemCombo(batata);
+        comboMaster.adicionarItemCombo(refri);
+
+        cardapio.cadastrarComboPrototype("COMBO_CASAL", comboMaster);
+
+
+        Combo comboDoCliente = cardapio.solicitarCombo("COMBO_CASAL");
+
+        comboDoCliente.removerItemCombo(batata);
+
+        comboDoCliente.adicionarItemCombo(new Bacon(new HamburguerCarne()));
+
+
+
+        assertEquals(4, comboDoCliente.getItensCombo().size());
+        assertTrue(comboDoCliente.getDescricao().contains("Bacon"));
+        assertFalse(comboDoCliente.getDescricao().contains("Batata Frita"));
+
+
+        Combo comboOriginalProtegido = cardapio.solicitarCombo("COMBO_CASAL");
+
+
+        assertEquals(4, comboOriginalProtegido.getItensCombo().size());
+        assertTrue(comboOriginalProtegido.getDescricao().contains("Batata Frita"));
+        assertFalse(comboOriginalProtegido.getDescricao().contains("Bacon"));
+    }
 }
