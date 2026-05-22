26d0db58e3cee73002bba624f94fbd9f2d325979
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-22 17:52:20 -0300

    Criando classe iteradora para filtrar fila de pedidos da cozinha - Iterator

--- src/main/java/hamburgueria/IteratorPedidosCozinha.java
+++ src/main/java/hamburgueria/IteratorPedidosCozinha.java
@@ -1,4 +1,45 @@
 package hamburgueria;
 
-public class IteratorPedidosCozinha {
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class IteratorPedidosCozinha implements Iterator<Pedido> {
+
+    private List<Pedido> pedidos;
+    private int posicaoAtual = 0;
+    private String filtroTipo;
+
+    public IteratorPedidosCozinha(List<Pedido> pedidos, String filtroTipo) {
+        this.pedidos = pedidos;
+        this.filtroTipo = filtroTipo;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        while (posicaoAtual < pedidos.size()) {
+            Pedido pedido = pedidos.get(posicaoAtual);
+
+            boolean tipoValido = (filtroTipo == null || pedido.getTipoPedido().equalsIgnoreCase(filtroTipo));
+            boolean naoCancelado = !pedido.isCancelado();
+
+            if (tipoValido && naoCancelado) {
+                return true;
+            }
+
+            posicaoAtual++;
+        }
+        return false;
+    }
+
+    @Override
+    public Pedido next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("Não há mais pedidos na fila.");
+        }
+        Pedido pedido = pedidos.get(posicaoAtual);
+        posicaoAtual++;
+        return pedido;
+    }
 }
