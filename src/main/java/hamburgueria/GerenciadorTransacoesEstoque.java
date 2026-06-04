6e6190059785e4ebd8fee9db26105822aedc8fa4
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-04 10:46:37 -0300

    Implementando o log e rollback de operações para controlar o estoque - Command

--- src/main/java/hamburgueria/GerenciadorTransacoesEstoque.java
+++ src/main/java/hamburgueria/GerenciadorTransacoesEstoque.java
@@ -1,4 +1,22 @@
 package hamburgueria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorTransacoesEstoque {
+
+    private List<ComandoEstoque> historicoTransacoes = new ArrayList<>();
+
+    public void processarTransacao(ComandoEstoque comando) {
+        comando.executar();
+        this.historicoTransacoes.add(comando);
+    }
+
+    public void estornarUltimaTransacao() {
+        if (!historicoTransacoes.isEmpty()) {
+
+            ComandoEstoque ultimoComando = historicoTransacoes.remove(historicoTransacoes.size() - 1);
+            ultimoComando.desfazer();
+        }
+    }
 }
