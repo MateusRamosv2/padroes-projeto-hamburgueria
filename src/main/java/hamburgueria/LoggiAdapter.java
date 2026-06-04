83b6e23906dd902865001dadec5cfc1c7fdf2e22
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-04 12:29:24 -0300

    implementando adaptador para traduzir o pedido para a api da loggi - Adapter

--- src/main/java/hamburgueria/LoggiAdapter.java
+++ src/main/java/hamburgueria/LoggiAdapter.java
@@ -1,4 +1,23 @@
 package hamburgueria;
 
-public class LoggiAdapter {
+public class LoggiAdapter implements IntegracaoLogistica {
+
+    private ApiLoggiExterna apiLoggi;
+    private final String apiKeyConfigurada;
+
+    public LoggiAdapter(ApiLoggiExterna apiLoggi) {
+        this.apiLoggi = apiLoggi;
+
+        this.apiKeyConfigurada = "TOKEN_SECRETO_LOGGI_2026";
+    }
+
+    @Override
+    public String despacharPedido(PedidoDelivery pedido, String enderecoCliente) {
+
+
+        float valorTotalDoPedido = pedido.calcularTotalFinal();
+
+
+        return apiLoggi.requestRider(this.apiKeyConfigurada, enderecoCliente, valorTotalDoPedido);
+    }
 }
