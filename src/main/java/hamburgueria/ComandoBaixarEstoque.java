8a2c68f38d0a73893afb31c3a93df8cd94ecdb23
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-04 10:44:37 -0300

    Criando o comando que baixa e garante o estorno de ingredientes - Command

--- src/main/java/hamburgueria/ComandoBaixarEstoque.java
+++ src/main/java/hamburgueria/ComandoBaixarEstoque.java
@@ -1,4 +1,25 @@
 package hamburgueria;
 
-public class ComandoBaixarEstoque {
+public class ComandoBaixarEstoque implements ComandoEstoque {
+    private EstoqueIngredientes estoque;
+    private String ingrediente;
+    private int quantidade;
+
+    public ComandoBaixarEstoque(EstoqueIngredientes estoque, String ingrediente, int quantidade) {
+        this.estoque = estoque;
+        this.ingrediente = ingrediente;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public void executar() {
+
+        estoque.darBaixa(ingrediente, quantidade);
+    }
+
+    @Override
+    public void desfazer() {
+
+        estoque.adicionarEstoque(ingrediente, quantidade);
+    }
 }
