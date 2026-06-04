eef8d19caa38d2fc6747c89c7e0a0c0a70aaa96d
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-04 10:50:01 -0300

    Criando o teste que vai baixar o estoque assim que o pedido for processado

--- src/test/java/hamburgueria/GerenciadorTransacoesEstoqueTest.java
+++ src/test/java/hamburgueria/GerenciadorTransacoesEstoqueTest.java
@@ -1,4 +1,33 @@
 package hamburgueria;
 
-public class GerenciadorTransacoesEstoqueTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GerenciadorTransacoesEstoqueTest {
+
+    private EstoqueIngredientes estoque;
+    private GerenciadorTransacoesEstoque gerenciador;
+
+    @BeforeEach
+    void setUp() {
+        estoque = new EstoqueIngredientes();
+        gerenciador = new GerenciadorTransacoesEstoque();
+
+
+        estoque.adicionarEstoque("Hambúrguer de Carne", 100);
+        estoque.adicionarEstoque("Pão Brioche", 100);
+    }
+
+    @Test
+    void deveDarBaixaNoEstoqueAoProcessarPedido() {
+
+        ComandoEstoque baixaCarne = new ComandoBaixarEstoque(estoque, "Hambúrguer de Carne", 2);
+        gerenciador.processarTransacao(baixaCarne);
+
+
+        assertEquals(98, estoque.consultarEstoque("Hambúrguer de Carne"));
+    }
+
 }
