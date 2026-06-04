c9ab1c519a619de588805a4aa14a53577a6e0add
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-04 12:27:53 -0300

    adicionando integração externa da Loggi

--- src/main/java/hamburgueria/ApiLoggiExterna.java
+++ src/main/java/hamburgueria/ApiLoggiExterna.java
@@ -1,4 +1,12 @@
 package hamburgueria;
 
 public class ApiLoggiExterna {
+
+
+    public String requestRider(String authKey, String destination, double totalAmount) {
+        if (authKey == null || authKey.isEmpty()) {
+            throw new SecurityException("Acesso negado: API Key inválida na Loggi.");
+        }
+        return "SUCESSO: Motoboy da Loggi a caminho de [" + destination + "]. Valor a cobrar: R$" + totalAmount;
+    }
 }
