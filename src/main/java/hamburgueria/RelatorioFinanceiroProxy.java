fa1753330aeb793eb4c70d6019fb2ff3abdac187
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-05 13:22:36 -0300

    Implementação do Proxy relatório

--- src/main/java/hamburgueria/RelatorioFinanceiroProxy.java
+++ src/main/java/hamburgueria/RelatorioFinanceiroProxy.java
@@ -1,4 +1,32 @@
 package hamburgueria;
 
-public class RelatorioFinanceiroProxy {
+import java.util.List;
+
+public class RelatorioFinanceiroProxy implements RelatorioFinanceiro {
+
+    private RelatorioFinanceiroReal relatorioReal;
+    private FuncionarioHamburgueria funcionarioLogado;
+    private String dataFechamento;
+
+    public RelatorioFinanceiroProxy(FuncionarioHamburgueria funcionarioLogado, String dataFechamento) {
+        this.funcionarioLogado = funcionarioLogado;
+        this.dataFechamento = dataFechamento;
+    }
+
+    @Override
+    public List<String> gerarRelatorioFaturamento() {
+
+
+        if (funcionarioLogado instanceof FuncionarioGerente || funcionarioLogado instanceof FuncionarioFinanceiro) {
+
+            if (this.relatorioReal == null) {
+                this.relatorioReal = new RelatorioFinanceiroReal(this.dataFechamento);
+            }
+            return this.relatorioReal.gerarRelatorioFaturamento();
+
+        } else {
+
+            throw new SecurityException("Acesso Negado: O cargo de " + funcionarioLogado.getCargo() + " não possui permissão para visualizar o faturamento.");
+        }
+    }
 }
