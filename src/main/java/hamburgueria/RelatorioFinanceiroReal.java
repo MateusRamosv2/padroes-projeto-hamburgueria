a7a41f26a30e922e6d35c85da560c9a828730dbd
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-05 13:21:29 -0300

    Implementando o relatório financeiro

--- src/main/java/hamburgueria/RelatorioFinanceiroReal.java
+++ src/main/java/hamburgueria/RelatorioFinanceiroReal.java
@@ -1,4 +1,26 @@
 package hamburgueria;
 
-public class RelatorioFinanceiroReal {
+import java.util.Arrays;
+import java.util.List;
+
+public class RelatorioFinanceiroReal implements RelatorioFinanceiro {
+    private String dataFechamento;
+
+    public RelatorioFinanceiroReal(String dataFechamento) {
+        this.dataFechamento = dataFechamento;
+
+        System.out.println("[Log BD] Calculando fluxo de caixa e faturamento para " + dataFechamento + "...");
+    }
+
+    @Override
+    public List<String> gerarRelatorioFaturamento() {
+
+        return Arrays.asList(
+                "Relatório Consolidado: " + dataFechamento,
+                "Receita Total: R$ 8.450,00",
+                "Custos de Ingredientes: R$ 2.100,00",
+                "Taxas de Delivery (Loggi): R$ 450,00",
+                "Lucro Líquido: R$ 5.900,00"
+        );
+    }
 }
