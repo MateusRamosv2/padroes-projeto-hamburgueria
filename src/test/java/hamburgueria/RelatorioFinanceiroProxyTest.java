34e42cacc3144817b13f08e48fe55552c6234e2b
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-05 13:23:49 -0300

    Implementando teste de acesso para gerente

--- src/test/java/hamburgueria/RelatorioFinanceiroProxyTest.java
+++ src/test/java/hamburgueria/RelatorioFinanceiroProxyTest.java
@@ -1,4 +1,19 @@
 package hamburgueria;
 
-public class RelatorioFinanceiroProxyTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RelatorioFinanceiroProxyTest {
+
+    @Test
+    void devePermitirAcessoAosDadosSigilososParaGerente() {
+
+        FuncionarioHamburgueria gerente = new FuncionarioGerente(null);
+
+        RelatorioFinanceiro proxy = new RelatorioFinanceiroProxy(gerente, "2026-06-05");
+
+
+        assertEquals(5, proxy.gerarRelatorioFaturamento().size());
+        assertTrue(proxy.gerarRelatorioFaturamento().get(1).contains("Receita Total: R$"));
+    }
 }
