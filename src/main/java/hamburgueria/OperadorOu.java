4ce5ff1b237df1b04378b927c3352fac6f854077
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-08 16:47:36 -0300

    Implementando operador OU - Interpreter

--- src/main/java/hamburgueria/OperadorOu.java
+++ src/main/java/hamburgueria/OperadorOu.java
@@ -1,4 +1,16 @@
 package hamburgueria;
 
-public class OperadorOu {
+public class OperadorOu implements FiltroPedidoExpressao {
+    private FiltroPedidoExpressao esquerda;
+    private FiltroPedidoExpressao direita;
+
+    public OperadorOu(FiltroPedidoExpressao esquerda, FiltroPedidoExpressao direita) {
+        this.esquerda = esquerda;
+        this.direita = direita;
+    }
+
+    @Override
+    public boolean interpretar(Pedido pedido) {
+        return esquerda.interpretar(pedido) || direita.interpretar(pedido);
+    }
 }
