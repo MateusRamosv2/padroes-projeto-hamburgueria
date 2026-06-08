42845091c5f0c86aa9b7e4c23eaa383d2e502635
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-08 16:44:18 -0300

    Implementando o primeiro filtro para pedidos

--- src/main/java/hamburgueria/FiltroTipoPedido.java
+++ src/main/java/hamburgueria/FiltroTipoPedido.java
@@ -1,4 +1,15 @@
 package hamburgueria;
 
-public class FiltroTipoPedido {
+public class FiltroTipoPedido implements FiltroPedidoExpressao {
+    private String tipoAlvo;
+
+    public FiltroTipoPedido(String tipoAlvo) {
+        this.tipoAlvo = tipoAlvo;
+    }
+
+    @Override
+    public boolean interpretar(Pedido pedido) {
+
+        return pedido.getTipoPedido().equalsIgnoreCase(tipoAlvo);
+    }
 }
