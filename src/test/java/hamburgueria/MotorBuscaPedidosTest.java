358f6e18e7a1212d6751658d46dc10e8cf127efb
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-08 16:51:06 -0300

    Adicionando teste de busca de pedidos

--- src/test/java/hamburgueria/MotorBuscaPedidosTest.java
+++ src/test/java/hamburgueria/MotorBuscaPedidosTest.java
@@ -1,4 +1,41 @@
 package hamburgueria;
 
-public class MotorBuscaPedidosTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MotorBuscaPedidosTest {
+
+    private Pedido pedidoCaroDelivery;
+    private Pedido pedidoBaratoBalcao;
+
+    @BeforeEach
+    void setUp() {
+
+        FormaPagamento cartao = new PagamentoCartao();
+
+        pedidoCaroDelivery = new PedidoBuilder()
+                .setTipoPedido("Delivery")
+                .setFormaPagamento(cartao)
+                .adicionarItem(new HamburguerCarne())
+                .adicionarItem(new HamburguerCarne())
+                .build();
+
+        pedidoBaratoBalcao = new PedidoBuilder()
+                .setTipoPedido("Balcao")
+                .setFormaPagamento(cartao)
+                .adicionarItem(new BatataFrita())
+                .build();
+    }
+
+    @Test
+    void deveEncontrarPedidoDeliveryCaro() {
+
+        MotorBuscaPedidos motor = new MotorBuscaPedidos("TIPO Delivery E VALOR_MAIOR_QUE 40");
+
+        assertTrue(motor.interpretar(pedidoCaroDelivery));
+        assertFalse(motor.interpretar(pedidoBaratoBalcao));
+    }
+
+
 }
