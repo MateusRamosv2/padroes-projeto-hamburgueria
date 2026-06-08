4e8be8e08cc3433f480fa98274e54856d50ce689
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-08 16:47:01 -0300

    Implementando operadores para junção de filtros de pesquisa - Interpreter

--- src/main/java/hamburgueria/OperadorE.java
+++ src/main/java/hamburgueria/OperadorE.java
@@ -1,4 +1,16 @@
 package hamburgueria;
 
-public class OperadorE {
+public class OperadorE implements FiltroPedidoExpressao {
+    private FiltroPedidoExpressao esquerda;
+    private FiltroPedidoExpressao direita;
+
+    public OperadorE(FiltroPedidoExpressao esquerda, FiltroPedidoExpressao direita) {
+        this.esquerda = esquerda;
+        this.direita = direita;
+    }
+
+    @Override
+    public boolean interpretar(Pedido pedido) {
+        return esquerda.interpretar(pedido) && direita.interpretar(pedido);
+    }
 }
