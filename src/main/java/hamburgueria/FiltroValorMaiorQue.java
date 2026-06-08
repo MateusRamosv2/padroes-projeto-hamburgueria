4f3550b7f1c8eeefae4776c7c663561a8711f97a
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-08 16:45:57 -0300

    Implementando mais um filtro base para pedidos

--- src/main/java/hamburgueria/FiltroValorMaiorQue.java
+++ src/main/java/hamburgueria/FiltroValorMaiorQue.java
@@ -1,4 +1,15 @@
 package hamburgueria;
 
-public class FiltroValorMaiorQue {
+public class FiltroValorMaiorQue implements FiltroPedidoExpressao {
+    private float valorAlvo;
+
+    public FiltroValorMaiorQue(float valorAlvo) {
+        this.valorAlvo = valorAlvo;
+    }
+
+    @Override
+    public boolean interpretar(Pedido pedido) {
+
+        return pedido.calcularTotalFinal() > this.valorAlvo;
+    }
 }
