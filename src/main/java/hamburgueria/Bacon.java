f48f061f3b596741239152698d2382d3cd3300cb
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-17 18:24:42 -0300

    Ajustando classe Bacon, removendo números mágicos

--- src/main/java/hamburgueria/Bacon.java
+++ src/main/java/hamburgueria/Bacon.java
@@ -1,13 +1,27 @@
 package hamburgueria;
 
 public class Bacon extends AdicionalDecorator {
-    public Bacon(Item itemDecorado) { super(itemDecorado); }
-    public String getNomeAdicional() { return "Bacon"; }
-    public float getPrecoAdicional() { return 5.0f; }
+
+
+    private static final String NOME_ADICIONAL = "Bacon";
+    private static final float PRECO_ADICIONAL = 5.0f;
+
+    public Bacon(Item itemDecorado) {
+        super(itemDecorado);
+    }
+
+    @Override
+    public String getNomeAdicional() {
+        return NOME_ADICIONAL;
+    }
+
+    @Override
+    public float getPrecoAdicional() {
+        return PRECO_ADICIONAL;
+    }
 
     @Override
     public String aceitar(VisitorItem visitor) {
         return visitor.visitar(this);
     }
-
 }
