42352872b52872f690d4af5eb065560dd19343f2
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-17 18:22:25 -0300

    Ajustando classe Refrigerante, retirando números mágicos

--- src/main/java/hamburgueria/Refrigerante.java
+++ src/main/java/hamburgueria/Refrigerante.java
@@ -1,14 +1,19 @@
 package hamburgueria;
 
 public class Refrigerante implements Item {
+
+
+    private static final String DESCRICAO_PADRAO = "Refrigerante Lata";
+    private static final float PRECO_BASE = 8.0f;
+
     @Override
     public float getPreco() {
-        return 8.0f;
+        return PRECO_BASE;
     }
 
     @Override
     public String getDescricao() {
-        return "Refrigerante Lata";
+        return DESCRICAO_PADRAO;
     }
 
     @Override
