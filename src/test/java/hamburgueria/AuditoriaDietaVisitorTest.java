822bcfcd49e0c0a79ed7319bc70b46b524c6fd68
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-18 18:05:37 -0300

    Ajustando teste Auditoria

--- src/test/java/hamburgueria/AuditoriaDietaVisitorTest.java
+++ src/test/java/hamburgueria/AuditoriaDietaVisitorTest.java
@@ -1,9 +1,27 @@
 package hamburgueria;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuditoriaDietaVisitorTest {
+
+    private String relatorioComboMonstro;
+
+    @BeforeEach
+    void setUp() {
+
+        Combo combo = new Combo("Combo Monstro", 0.10f);
+        combo.adicionarItemCombo(new Queijo(new HamburguerCarne()));
+        combo.adicionarItemCombo(new BatataFrita());
+        combo.adicionarItemCombo(new Refrigerante());
+
+        AuditoriaDietaVisitor auditor = new AuditoriaDietaVisitor();
+
+        relatorioComboMonstro = combo.aceitar(auditor);
+    }
+
+
 
     @Test
     void deveAuditarHamburguerComBacon() {
@@ -11,30 +29,29 @@ class AuditoriaDietaVisitorTest {
         AuditoriaDietaVisitor auditor = new AuditoriaDietaVisitor();
 
         String resultado = lanche.aceitar(auditor);
-
         assertEquals("[Carnívoro/Porco] Adicional de Bacon -> [Carnívoro] Hambúrguer Bovino Tradicional", resultado);
     }
 
+
+
     @Test
-    void deveAuditarComboInteiroEVerificarAlergenicos() {
+    void auditoriaDeComboDeveConterCabecalhoComNomeDoCombo() {
+        assertTrue(relatorioComboMonstro.contains("--- Auditoria do Combo Monstro"));
+    }
 
-        Combo combo = new Combo("Combo Monstro", 0.10f);
+    @Test
+    void auditoriaDeComboDeveIdentificarAdicionalDeQueijoComoLaticinio() {
+        assertTrue(relatorioComboMonstro.contains("[Vegetariano/Laticínio] Adicional de Queijo"));
+    }
 
+    @Test
+    void auditoriaDeComboDeveIdentificarHamburguerDeCarneComoCarnivoro() {
+        assertTrue(relatorioComboMonstro.contains("[Carnívoro] Hambúrguer Bovino Tradicional"));
+    }
 
-        combo.adicionarItemCombo(new Queijo(new HamburguerCarne()));
-
-
-        combo.adicionarItemCombo(new BatataFrita());
-        combo.adicionarItemCombo(new Refrigerante());
-
-        AuditoriaDietaVisitor auditor = new AuditoriaDietaVisitor();
-        String relatorioTotem = combo.aceitar(auditor);
-
-
-        assertTrue(relatorioTotem.contains("--- Auditoria do Combo Monstro"));
-        assertTrue(relatorioTotem.contains("[Vegetariano/Laticínio] Adicional de Queijo"));
-        assertTrue(relatorioTotem.contains("[Carnívoro] Hambúrguer Bovino Tradicional"));
-        assertTrue(relatorioTotem.contains("[Vegano] Batata Frita"));
+    @Test
+    void auditoriaDeComboDeveIdentificarBatataFritaComoVegana() {
+        assertTrue(relatorioComboMonstro.contains("[Vegano] Batata Frita"));
     }
 
 }
